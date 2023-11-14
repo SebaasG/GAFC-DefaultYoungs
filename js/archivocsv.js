e29c56e0e9bb691eed5d619a08e57e1f54359098
SebaasG <miguel.poveda1220@gmail.com>
2023-11-14 10:34:33 -0500

    Correcciones y Comentarios

--- js/archivocsv.js
+++ js/archivocsv.js
@@ -1,3 +1,4 @@
+// Declaración de variables globales
 let lineasDatos;
 let nombres = [];
 let codigosFicha = [];
@@ -6,41 +7,49 @@ let fechasInicio = [];
 let tiposPrograma = [];
 let ipv1 = false, ipv2 = false;
 
+// Definición de constantes para el rango de códigos generados
 const rangoMinimo = 10000;
 const rangoMaximo = 10000000;
+
+// Arreglo para almacenar los números generados
 const numerosGeneradosArray = [];
 
+// Función asincrónica para generar códigos aleatorios
 async function generarCodigos() {
+    // Verificar si se han generado todos los códigos posibles en el rango
     if (numerosGeneradosArray.length === rangoMaximo - rangoMinimo + 1) {
-
+        // Si ya se generaron todos los códigos posibles, salir de la función
         return;
     }
-
+    // Calcular la cantidad de registros que faltan por generar
     const registrosPorGenerar = nombres.length - numerosGeneradosArray.length;
 
+    // Bucle para generar códigos aleatorios
     for (let i = 0; i < registrosPorGenerar; i++) {
         let numeroAleatorio;
         let existeEnDB;
-
+        // Generar un número aleatorio y verificar si ya existe en el arreglo o en la base de datos
         do {
+            // Generar un número aleatorio dentro del rango especificado
             numeroAleatorio = Math.floor(Math.random() * (rangoMaximo - rangoMinimo + 1)) + rangoMinimo;
+            // Verificar si el número aleatorio ya está en el arreglo generado o en la base de datos
             existeEnDB = await existeEnBaseDeDatos(numeroAleatorio);
         } while (numerosGeneradosArray.includes(numeroAleatorio) || existeEnDB);
 
+        // Agregar el número aleatorio generado al arreglo
         numerosGeneradosArray.push(numeroAleatorio);
     }
 }
 
+
 async function existeEnBaseDeDatos(numeroAleatorio) {
     try {
-        const response = await fetch('http://localhost:8085/apiCita/Cod', {
-            method: 'GET'
+        const response = await fetch('http://localhost:8085/apiCita/Cod', { // esto verifica si existe en la base de datos
+            method: 'GET' //El tipo de request que va a hacer 
         });
-
-        if (response.ok) {
-            const data = await response.json();
-            // Comprobar si el númeroAleatorio no está en la lista de números de la base de datos
-            const existe = data.includes(numeroAleatorio);
+        if (response.ok) {//verifica que todos los datos lleguen correctamente
+            const data = await response.json();//Guarda la respuesta en una variable
+            const existe = data.includes(numeroAleatorio);  // Comprobar si el númeroAleatorio no está en la lista de números de la base de datos
             return existe;
         } else {
             console.error('Error en la solicitud HTTP');
@@ -51,11 +60,10 @@ async function existeEnBaseDeDatos(numeroAleatorio) {
         return false;
     }
 }
-
-function leerArchivo(e) {
-    const archivo = e.target.files[0];
-    if (!archivo) {
-        return;
+function leerArchivo(e) {// Función para leer el contenido de un archivo seleccionado por el usuario
+    const archivo = e.target.files[0]; // Obtener el primer archivo seleccionado en el evento
+    if (!archivo) {  // Verificar si no se seleccionó ningún archivo
+        return; // Si no hay archivo, salir de la función
     }
 
     // Reiniciar los arreglos antes de cargar nuevos datos
@@ -66,21 +74,27 @@ function leerArchivo(e) {
     tiposPrograma = [];
 
     const lector = new FileReader();
+
+    // Configurar el evento onload, que se ejecutará cuando la lectura del archivo sea exitosa
     lector.onload = function (e) {
         // Almacenar todas las líneas de datos en un arreglo
         lineasDatos = e.target.result.split('\n');
-
+        // Iterar sobre cada línea de datos
         lineasDatos.forEach(function (linea) {
+            // Definir un patrón de separación utilizando coma o punto y coma
             const separador = /,|;/;
             const datos = linea.split(separador);
 
+            // Separar los datos de la línea utilizando el patrón de separación
             if (datos.length >= 5) {
+                // Limpiar y extraer cada dato de la línea
                 const nombre = datos[0].replace(/"/g, "").trim();
                 const codigoFicha = datos[1].replace(/"/g, "").trim();
                 const documento = datos[2].replace(/"/g, "").trim();
                 const fechaInicio = datos[3].replace(/"/g, "").trim();
                 const tipoPrograma = datos[4].replace(/"/g, "").trim();
 
+                // Agregar los datos a los respectivos arreglos
                 nombres.push(nombre);
                 codigosFicha.push(codigoFicha);
                 documentos.push(documento);
@@ -88,53 +102,41 @@ function leerArchivo(e) {
                 tiposPrograma.push(tipoPrograma);
             }
         });
-
-        // Ahora tienes los datos separados en diferentes arreglos
-        console.log(nombres);
-        console.log(codigosFicha);
-        console.log(documentos);
-        console.log(fechasInicio);
-        console.log(tiposPrograma);
     };
-
+    // Iniciar la lectura del archivo como texto
     lector.readAsText(archivo);
 }
 
-function Cbo() {
-    const cbo = document.getElementById("cbo-prueba").value;
+function Cbo() {    // Obtener el valor seleccionado del elemento con el id "cbo-prueba"
 
-    if (cbo == 1) {
-        console.log(" Se seleccionó la prueba 1 ")
+    const cbo = document.getElementById("cbo-prueba").value; // Verificar el valor seleccionado y realizar acciones correspondientes
+    if (cbo == 1) {// Si el valor es 1, actualizar variables
         ipv1 = true;
         ipv2 = false;
-        console.log(ipv1, ipv2)
-    } else if (cbo == 2) {
-        console.log(" Se seleccionó la prueba 2 ")
+    } else if (cbo == 2) { // Si el valor es 2, actualizar variables
         ipv1 = false;
         ipv2 = true;
-        console.log(ipv1, ipv2)
-    } else if (cbo == 3) {
-        console.log(" Se seleccionaron ambas pruebas ")
+    } else if (cbo == 3) {// Si el valor es 3, actualizar variables 
         ipv1 = true;
         ipv2 = true;
-        console.log(ipv1, ipv2)
     } else {
-        console.log(" No se seleccionó nada ")
+        // Si no es ninguno de los casos anteriores, mostrar en la consola
+        console.log(" No se seleccionó nada ");
     }
 }
 
+//Funcion para guardar los datos que viene en el archivo csv
 async function enviarRegistros() {
 
-
-    for (let index = 0; index < nombres.length; index++) {
+    for (let index = 0; index < nombres.length; index++) { //Se coloca nombres.length para que solo se haga hasta el número de aprendices
         const response = await fetch("http://localhost:8085/apiApe/save", {
             method: "POST",
             body: JSON.stringify({
-                "docApe": documentos[index],
-                "nomApe": nombres[index],
-                "tipPro": tiposPrograma[index],
-                "fecIni": fechasInicio[index],
-                "codFic": codigosFicha[index]
+                "docApe": documentos[index], //se manda uno por uno del documento
+                "nomApe": nombres[index], //se manda uno por uno los nombres
+                "tipPro": tiposPrograma[index], //se manda uno por el tipo de programa al que pertenece
+                "fecIni": fechasInicio[index], //se manda una por una la fecha
+                "codFic": codigosFicha[index] //se manda uno por uno el código de la ficha
             }),
             headers: {
                 "Content-Type": "application/json"
@@ -142,28 +144,30 @@ async function enviarRegistros() {
         });
 
         if (response.ok) {
-            console.log(`Registro ${index + 1} enviado con éxito`);
+            console.log(`Registro ${index + 1} enviado con éxito`);//Respuesta por si fue exitosa la operación
         } else {
-            console.error(`Error al enviar registro ${index + 1}`);
+            console.error(`Error al enviar registro ${index + 1}`);//Respuesta por si fue erronea la operación
         }
     }
-
-    // console.log('Todos los registros han sido enviados');
-    // Swal.fire(' ¡Todos los registros han sido enviados! ' , 'success');
-    // location.reload();
     asginarPrueba();
 }
+// Función asincrónica para asignar pruebas
 async function asginarPrueba() {
+    // Verificar si se han generado todos los números aleatorios necesarios
     if (numerosGeneradosArray.length < nombres.length) {
         console.log('Esperando a que se generen todos los números aleatorios...');
+        // Si no se han generado todos los números, salir de la función
         return;
     }
-    var docIns = localStorage.getItem("datos");
-    for (let index = 0; index < nombres.length; index++) {
-        const response = await fetch("http://localhost:8085/apiCita/save", {
+
+    var docIns = localStorage.getItem("datos");// Obtener el documento del instructor almacenado en el localStorage
+
+    for (let index = 0; index < nombres.length; index++) {// Iterar sobre los nombres y realizar operaciones para cada índice
+        const response = await fetch("http://localhost:8085/apiCita/save", {        // Enviar una solicitud POST al servidor para guardar datos
+
             method: "POST",
             body: JSON.stringify({
-
+                // Datos a enviar en el cuerpo de la solicitud
                 "docApe": documentos[index],
                 "docIns": docIns,
                 "codIng": numerosGeneradosArray[index],
@@ -171,16 +175,19 @@ async function asginarPrueba() {
                 "ipv2": ipv2
             }),
             headers: {
+                // Encabezados de la solicitud
                 "Content-Type": "application/json"
             }
         });
 
-        if (response.ok) {
+        if (response.ok) {  // Verificar si la respuesta de la solicitud es exitosa 
             console.log(`Registro ${index + 1} enviado con éxito`);
         } else {
-            console.error(`Error al enviar registro ${index + 1}`);
+            console.error(`Error al enviar registro ${index + 1}`); // En caso de error, mostrar mensaje en la consola
         }
     }
+
+
 
     // Swal.fire({
     //     position: 'center',
@@ -198,15 +205,35 @@ async function asginarPrueba() {
     // location.reload();
 }
 
-
 document.addEventListener("DOMContentLoaded", function () {
+
     const btn = document.getElementById("hola");
-    btn.addEventListener("click", async function () {
+
+    btn.addEventListener("click", async function () {// se le coloca un evento para que cuando se le de click se llamen las funciones
+
+        const cbo = document.getElementById("cbo-prueba").value;
+        if (cbo === "0") {// Verificar si se seleccionó una opción
+            Swal.fire({  // Si no se seleccionó ninguna opción, mostrar en la consola y salir de la función
+                position: 'center',
+                allowOutsideClick: false,
+                allowEscapeKey: false,
+                allowEnterKey: false,
+                stopKeydownPropagation: true,
+                title: '¡ ERROR !',
+                html: '<b class="texto-alerta"> ¡Debe seleccionar una opcion! </b>',
+                icon: 'error',
+                iconColor: '#0e810e',
+                confirmButtonColor: '#0e810e',
+                timer: 8000
+            });
+            document.getElementById("cbo-prueba").focus();
+            return;
+        }
         Cbo();
         await generarCodigos();
-        if (numerosGeneradosArray.length === nombres.length) {
+        if (numerosGeneradosArray.length === nombres.length) {//esto valida que solo se envien registros cuando este todo listo
             enviarRegistros();
-            Swal.fire({
+            Swal.fire({//alerta de éxito
                 position: 'center',
                 allowOutsideClick: false,
                 allowEscapeKey: false,
@@ -220,7 +247,7 @@ document.addEventListener("DOMContentLoaded", function () {
                 timer: 8000
             });
             setTimeout(() => {
-                location.href = 'procesosInstructor.html';
+                location.href = 'procesosInstructor.html';//al terminar lo envia a el formulario de procesos instructor
             }, 2000);
 
         } else {
@@ -239,7 +266,6 @@ document.addEventListener("DOMContentLoaded", function () {
             });
         }
     });
-
     document.querySelector('#archivo1').addEventListener('change', leerArchivo, false);
 });
 
